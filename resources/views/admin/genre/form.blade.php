96fc5991de57181f2899d8c39ecb981cacb97ab3
Andri <andri.haltec@gmail.com>
2025-05-29 22:16:20 +0700

    #fix : slug genre

--- resources/views/admin/genre/form.blade.php
+++ resources/views/admin/genre/form.blade.php
@@ -1,9 +1,9 @@
 <div class="error-message"></div>
 <div class="form-group">
     <label for="nama">Nama</label>
-    <input type="text" class="form-control" name="nama" value="{{ $data->name ?? '' }}" id="nama">
+    <input type="text" class="form-control" name="nama" slug-source value="{{ $data->name ?? '' }}" id="nama">
 </div>
 <div class="form-group">
     <label for="slug">Slug</label>
-    <input type="text" class="form-control" name="slug" value="{{ $data->slug ?? '' }}" id="slug">
+    <input type="text" class="form-control" name="slug" slug-target value="{{ $data->slug ?? '' }}" id="slug" readonly="readonly">
 </div>
